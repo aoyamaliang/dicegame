82ef9f01d7062be56070e7d2328c9c61af289c91
aoyamaliang <aoyamaliang@gmail.com>
2020-01-16 23:05:15 +0800

    遊戲結束後不能再按

--- app.js
+++ app.js
@@ -9,7 +9,7 @@ GAME RULES:
 
 */
 
-var scores, roundScore, activePlayer;
+var scores, roundScore, activePlayer, gamePlaying;
 
 init();
 /*骰子任意投出數字*/
@@ -18,45 +18,50 @@ init();
 
 
 document.querySelector('.btn-roll').addEventListener('click', function () {
+    if (gamePlaying) {
+        //1. Random numer
+        var dice = Math.floor(Math.random() * 6) + 1;
 
-    //1. Random numer
-    var dice = Math.floor(Math.random() * 6) + 1;
-
-    //2. Display the result
-    var diceDOM = document.querySelector('.dice');
-    diceDOM.style.display = 'block';
-    diceDOM.src = 'dice-' + dice + '.png';
+        //2. Display the result
+        var diceDOM = document.querySelector('.dice');
+        diceDOM.style.display = 'block';
+        diceDOM.src = 'dice-' + dice + '.png';
 
 
-    //3. Update the round score IF the rolled number was NOT a 1
-    /* !== not equal and/or not of the same type*/
-    if (dice !== 1) {
-        //Add score
-        roundScore += dice;
-        document.querySelector('#current-' + activePlayer).textContent = roundScore;
-    } else {
-        //Next player
-        nextPlayer();
+        //3. Update the round score IF the rolled number was NOT a 1
+        /* !== not equal and/or not of the same type*/
+        if (dice !== 1) {
+            //Add score
+            roundScore += dice;
+            document.querySelector('#current-' + activePlayer).textContent = roundScore;
+        } else {
+            //Next player
+            nextPlayer();
+        }
     }
 });
 
 document.querySelector('.btn-hold').addEventListener('click', function () {
-    //ADD CURRENT score to GLOBAL score
-    scores[activePlayer] += roundScore;
+    if (gamePlaying) {
+        //ADD CURRENT score to GLOBAL score
+        scores[activePlayer] += roundScore;
 
-    //Update the UI
-    document.querySelector('#score-' + activePlayer).textContent = scores[activePlayer];
-    //Check if player won the game
-    if (scores[activePlayer] >= 20) {
-        document.querySelector('#name-' + activePlayer).textContent = 'Winner!';
-        document.querySelector('.dice').style.display = 'none';
-        document.querySelector('.player-' + activePlayer + '-panel').classList.add("winner");
-        document.querySelector('.player-' + activePlayer + '-panel').classList.remove("winner");
-    } else {
+        //Update the UI
+        document.querySelector('#score-' + activePlayer).textContent = scores[activePlayer];
+        //Check if player won the game
+        if (scores[activePlayer] >= 20) {
+            document.querySelector('#name-' + activePlayer).textContent = 'Winner!';
+            document.querySelector('.dice').style.display = 'none';
+            document.querySelector('.player-' + activePlayer + '-panel').classList.add("winner");
+            document.querySelector('.player-' + activePlayer + '-panel').classList.remove("winner");
+            gamePlaying = false;
+        } else {
 
-        //Next player
-        nextPlayer();
+            //Next player
+            nextPlayer();
+        }
     }
+
 });
 
 function nextPlayer() {
@@ -88,6 +93,7 @@ function init() {
     scores = [0, 0];
     activePlayer = 0;
     roundScore = 0;
+    gamePlaying = true;
 
     /* 修改CSS 將骰子的style display 改為none,使骰子不顯示*/
     document.querySelector('.dice').style.display = 'none';
